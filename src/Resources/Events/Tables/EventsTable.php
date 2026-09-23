6d7b04ef82ea2a4687b79d1e8f41205ad51b52ba
Matondo João <matondojoaokitemoco2002@gmail.com>
2026-09-23 12:51:54 -0300

    Update Action namespaces to Filament\Actions for Filament v5 compatibility

--- src/Resources/Events/Tables/EventsTable.php
+++ src/Resources/Events/Tables/EventsTable.php
@@ -3,11 +3,11 @@
 namespace Matondojk\FilamentCalendar\Resources\Events\Tables;
 
 use Carbon\Carbon;
-use Filament\Tables\Actions\Action;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ViewAction;
+use Filament\Actions\Action;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
